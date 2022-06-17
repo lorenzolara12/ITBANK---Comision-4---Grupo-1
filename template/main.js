48dc2ce1b0767067342c273ca56366ab4ec1633c
juanma38 <juanpolimeni1@gmail.com>
2022-06-17 17:34:49 -0300

    fechas y acentos

--- template/main.js
+++ template/main.js
@@ -3,6 +3,7 @@ let compra= document.querySelectorAll('#valorCompra');
 let venta= document.querySelectorAll('#valorVenta');
 let variacion= document.querySelectorAll('#variacion');
 console.log(compra)
+let actualizacion = document.querySelectorAll("#ultimaActualizacion")
 
 function imprimir(arr, data, atrib)
 {
@@ -12,7 +13,8 @@ function imprimir(arr, data, atrib)
         elem.innerHTML= `$${data[i].casa[atrib]}`
         console.log( data[i].casa[atrib]);
         i++;
-    }  
+    }
+
 }
 
 fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
@@ -22,13 +24,20 @@ fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
    imprimir(compra, data, 'compra' );
     
    imprimir(venta, data, 'venta');
+    
+    let fecha = new Date
+    console.log (fecha)
 
+   for (let elem of actualizacion){
+    elem.innerHTML= `ACTUALIZADO ${fecha.getDate()}/${fecha.getMonth()}/${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()} `
+
+   }
    
 
    let i = 0;
     for(let elem of variacion)
     {
-        elem.innerHTML= `VARIACION ${data[i].casa.variacion} % `
+        elem.innerHTML= `VARIACIÓN ${data[i].casa.variacion} % `
         console.log(data[i].casa.variacion);
         i++;
     }  
